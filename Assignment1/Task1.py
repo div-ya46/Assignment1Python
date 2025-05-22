2bf2697a661b0fcbe5f74dc26f6c83e6980736ab
div-ya46 <divyasuthar462002@gmail.com>
2025-05-22 22:48:06 +0530

    Add files via upload

---
+++ Assignment1/Task1.py
@@ -0,0 +1,16 @@
+#Task 1:
+
+a=int(input('Enter the first number: '))
+b=int(input('Enter the second number: '))
+
+
+sum=a+b 
+sub=a-b
+mul=a*b
+div=a/b
+
+
+print('Addition: ',sum)
+print('Subtraction: ',sub)
+print('Multiplication: ',mul)
+print('division: ',div)
---
+++ Assignment1/Task2.py
@@ -0,0 +1,6 @@
+fn=input('Enter your first name: ')
+ln=input('Enter your last name: ')
+
+fl=fn+' '+ln+'!'
+
+print('Hello,',fl,'Welcome to the Python program.')
